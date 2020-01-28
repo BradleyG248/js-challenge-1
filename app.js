7b93db46e16dddd980fe0e49d3e4fc33e27cea36
BradleyG248 <calvinandhobbs28@gmail.com>
2020-01-28 09:23:52 -0700

    added objects

---
+++ app.js
@@ -0,0 +1,17 @@
+let snackBin = {
+  bin1: "trailMix",
+  bin2: "candy",
+  bin3: "krispyTreat",
+  bin4: "granolaBar",
+}
+let table = {
+  chair1: "blue",
+  chair2: "red",
+  chair3: {
+    brand: "ChairCo",
+    color: "cyan",
+    material: "Spring Steel",
+    ergonomics: "40% median",
+    fanbase: ["Earl", "John", "Larry", "Mr. Business"]
+  }
+}
